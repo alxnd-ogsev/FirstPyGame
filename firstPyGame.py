51558e01c3429361c8e606be61a00b3b9b2bdec4
alxnd-ogsev <alexandrenge.2@gmail.com>
2022-01-28 13:09:49 -0300

    Image file added.

--- firstPyGame.py
+++ firstPyGame.py
@@ -3,22 +3,56 @@ import pygame
 pygame.init()
 
 screenWidth = 500
-screenHeight = 500
+screenHeight = 480
 
 win = pygame.display.set_mode((screenWidth, screenHeight))
 
 pygame.display.set_caption("First Game")
 
+# Character images
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'), pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'), pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'), pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'), pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
+
+clock = pygame.time.Clock()
+
 # Character atribute
 x = 50
-y = 50
-width = 40
-height = 60
+y = 400
+width = 64
+height = 64
 vel = 5
+isJump = False
+jumpCount = 10
+left = False
+right = False
+walkCount = 0
 
+def redrawGameWindow():
+    global walkCount
+    win.blit(bg, (0,0))
+    
+    if walkCount + 1 >= 27:
+        walkCount = 0
+
+    if left:
+        win.blit(walkLeft[walkCount//3], (x,y))
+        walkCount += 1
+
+    elif right:
+        win.blit(walkRight[walkCount//3], (x,y))
+        walkCount += 1
+
+    else:
+        win.blit(char, (x,y))
+        
+    pygame.display.update()
+
+#mainloop
 run = True
 while run:
-    pygame.time.delay(100) # Clock (ms)
+    clock.tick(27) # Clock (ms)
     # Check for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # If red x is pressed
@@ -28,22 +62,41 @@ while run:
 
     if keys[pygame.K_LEFT] and x > vel:
         x -= vel
+        left = True
+        right = False
 
-    if keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
+    elif keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
         x += vel
+        right = True
+        left = False
 
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
+    else:
+        right = False
+        left = False
+        walkCount = 0
 
-    if keys[pygame.K_DOWN] and y < screenHeight - height - vel:
-        y += vel
+    if not(isJump):
+
+       if keys[pygame.K_SPACE]:
+            isJump = True
+            right = False
+            left = False
+            walkCount = 0
+            
+    else:
+
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+
+        else:
+            isJump = False
+            jumpCount = 10            
 
 
-    win.fill((0,0,0))
+    redrawGameWindow()
     
-
-    # Charact representation
-    pygame.draw.rect(win, (255,0,0), (x, y, width, height)) # Create a rectangle in the window which is red and has
-    pygame.display.update()
-
 pygame.quit()
