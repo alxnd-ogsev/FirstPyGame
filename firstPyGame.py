cb7763d614e8de0ab1f4e8d4b28d83e21ccd631d
alxnd-ogsev <alexandrenge.2@gmail.com>
2022-02-02 18:13:08 -0300

    Pygame Tutorial #9- Sound Effects, Music & More Collision completed!

--- firstPyGame.py
+++ firstPyGame.py
@@ -17,6 +17,12 @@ char = pygame.image.load('standing.png')
 
 clock = pygame.time.Clock()
 
+bulletSound = pygame.mixer.Sound('bullet.wav')
+hitSound = pygame.mixer.Sound('hit.wav')
+
+music = pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.play(-1)
+
 score = 0
 
 class player(object):
@@ -55,6 +61,23 @@ class player(object):
             self.hitbox = (self.x + 17, self.y + 11, 29, 52)
             #pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
 
+        def hit(self):
+                self.x = 60
+                self.y = 410
+                self.walkCount = 0
+                font1 = pygame.font.SysFont('comicsans', 100)
+                text = font1.render('-5', 1, (255,0,0))
+                win.blit(text, ((screenWidth/2)-(text.get_width()/2), (screenHeight/2)))
+                pygame.display.update()
+                i = 0
+                while i < 100:
+                        pygame.time.delay(10)
+                        i += 1
+                        for event in pygame.event.get():
+                                if event.type == pygame.QUIT:
+                                        i = 101
+                                        pyhame.quit()
+                
 class projectile(object):
     def __init__(self, x, y, radius, color, facing):
         self.x = x
@@ -81,7 +104,7 @@ class enemy(object):
                 self.walkCount = 0
                 self.vel = 3
                 self.hitbox = (self.x + 17, self.y + 2, 31, 57)
-                self.health = 9
+                self.health = 10
                 self.visible = True
 
         def draw(self,win):
@@ -97,9 +120,11 @@ class enemy(object):
                                 self.walkCount += 1
 
                         pygame.draw.rect(win, (255,0,0), (self.hitbox[0], self.hitbox[1] - 20, 50, 10))
-                        pygame.draw.rect(win, (0,128,0), (self.hitbox[0], self.hitbox[1] - 20, 50 - ((50/9) * (9 - self.health)), 10))                 
+                        pygame.draw.rect(win, (0,128,0), (self.hitbox[0], self.hitbox[1] - 20, 50 - ((5) * (10 - self.health)), 10))
                         self.hitbox = (self.x + 17, self.y + 2, 31, 57)
                         #pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
+                else:
+                        self.hitbox = (0,0,0,0)
 
         def move(self):
                 if self.vel > 0:
@@ -143,6 +168,12 @@ run = True
 while run:
     clock.tick(27) # Clock (ms)
 
+    if man.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and man.hitbox[1] + man.hitbox[3] > goblin.hitbox[1]:
+            if man.hitbox[0] + man.hitbox[2] > goblin.hitbox[0] and man.hitbox[0] - man.hitbox[2] < goblin.hitbox[0] + goblin.hitbox[2]:
+                    man.hit()
+                    if goblin.visible:
+                            score -= 5
+                            
     if shootLoop > 0:
             shootLoop += 1
     if shootLoop > 3:
@@ -156,9 +187,11 @@ while run:
     for bullet in bullets:
         if bullet.y - bullet.radius < goblin.hitbox[1] + goblin.hitbox[3] and bullet.y + bullet.radius > goblin.hitbox[1]:
                 if bullet.x + bullet.radius > goblin.hitbox[0] and bullet.x - bullet.radius < goblin.hitbox[0] + goblin.hitbox[2]:
+                        hitSound.play()
                         goblin.hit()
-                        score += 1
-                        bullets.pop(bullets.index(bullet))
+                        if goblin.visible:
+                                score += 1
+                                bullets.pop(bullets.index(bullet))
                 
         if bullet.x < screenWidth and bullet.x > 0:
             bullet.x += bullet.vel
@@ -168,6 +201,7 @@ while run:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_SPACE] and shootLoop == 0:
+        bulletSound.play()
         if man.left:
             facing = -1
         else:
